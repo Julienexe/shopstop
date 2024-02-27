3bbcb0b279edbd59eeb28b7097b0583620c9e1a3
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-02-27 22:49:57 +0300

    models

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from PIL import Image
 
 from .managers import CustomUserManager
 
@@ -16,3 +17,25 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True, blank=True)
+    image = models.ImageField(default='avatar.png',upload_to='profile/', blank=True)  
+    contact = models.CharField( max_length= 50, default = '+256755565556')
+    name = models.CharField(max_length = 255, default= 'user')
+
+    def __str__(self):
+        return f'{self.user.email} Profile'
+
+    def save(self, *args, **kwargs):
+        # save the profile first
+        super().save(*args, **kwargs)
+
+        # resize the image
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            # create a thumbnail
+            img.thumbnail(output_size)
+            # overwrite the larger image
+            img.save(self.image.path)
