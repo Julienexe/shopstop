2e268f21c17345674b5840c291e31bfa0768f649
@Alidat <alvinnahereza@gmail.com>
2024-02-28 11:19:59 +0300

    rt

--- shopstop/urls.py
+++ shopstop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'shop'
 urlpatterns = [
     path('business/register/', views.business_register, name='business_register'),
     path('menu/manage/', views.menu_management, name='menu_management'),
--- shopstop/views.py
+++ shopstop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.http import HttpResponse
+from django.template import loader
+
+def service_management(request):
+    template = loader.get_template('services.html')
+    return HttpResponse(template.render())
+
+def menu_management(request):
+    template = loader.get_template('menu.html')
+    return HttpResponse(template.render())
+
+def business_register(request):
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render())
+
