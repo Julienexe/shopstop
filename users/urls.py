44835e092a1eafc51b156aab3d73660ba523ec25
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-02-29 12:03:05 +0300

    untested registration

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,6 @@ app_name = "users"
 urlpatterns= [
     # 用户登录
     path('login/', views.user_login, name='user_login'),
+    #signup
+    path('signup',views.signup,name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from .forms import UserCreationForm, LoginForm
+from .forms import CustomUserCreationForm, LoginForm
 
 # login page
 def user_login(request):
@@ -17,3 +17,16 @@ def user_login(request):
         form = LoginForm()
     #return render(request, 'login.html', {'form': form})
 
+def  signup(request):
+    if request.method=='POST':
+        form =CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email') 
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=email, password=raw_password)
+            login(request,user)
+            #return redirect ('home')
+    else:
+       form = CustomUserCreationForm()     
+    #return render(request,'signup.html',{'form':form})  
