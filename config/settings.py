e534de0e88bb82cd530e05146540c77b54af8768
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-03-01 16:14:24 +0300

    galaxy

--- config/settings.py
+++ config/settings.py
@@ -120,12 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_FILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
-    )
-STATIC_ROOT = (
-    os.path.join(BASE_DIR,'staticfiles'),
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
