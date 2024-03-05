2e8bfa97c1a729ecf7b13d7035b04185a40f9924
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-03-05 19:29:37 +0300

    change form display layout

--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django import forms
 
@@ -9,6 +10,9 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = ''
 
 
 class CustomUserChangeForm(UserChangeForm):
