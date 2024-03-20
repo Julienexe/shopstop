fb519aefcf4ab53fb082a5f3e44b1c7e633daf6d
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-03-21 01:44:26 +0300

    testing form

---
+++ shopstop/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Item
+
+class  ItemModelForm(ModelForm):
+    class Meta:
+        model = Item
+        fields=('item_name','price','photos',)
--- shopstop/views.py
+++ shopstop/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from .models import Service, Item, Business
+from .forms import ItemModelForm
 
 
 def business_management(request, business_id):
@@ -11,12 +12,28 @@ def business_management(request, business_id):
     context = {"services":services, "items":items, "business" : business}
 
     return render(request,'shop/business-admin.html', context)
+
+def add_item(request):
+    if request.method == 'POST': # If the form has been submitted...
+        form = ItemModelForm(request.POST) # A form bound to the POST data
+        if form.is_valid(): # All validation rules pass
+            item = form.save()
+            return redirect('shop:shop')
+    else: 
+        form = ItemModelForm() # An unbound form
+    
+    return render(request, 'shop/product/add-item.html', {'form': form})
+
     
 def shop(request):
         items = Item.objects.all()
         services = Service.objects.all()
         context = {'items':items,'services':services}
         return render(request, "shop/home.html", context)
+
+def product_detail(request, item_id):
+     item = Item.objects.get(id = item_id)
+     return render(request, 'shop/product/product_detail.html', {'item':item})
 
 def home(request):
     return render(request, 'shop/index.html')
