d859cd2e53039a44fde4c4575fb84e8c84adbaa3
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-03-25 03:19:49 +0300

    add order models

--- shopstop/models.py
+++ shopstop/models.py
@@ -45,3 +45,18 @@ class Business_photos_videos(models.Model):  #Businesses can upload their photos
 
     def __str__(self):
         return f'{self.business}'  
+    
+class Order(models.Model):
+    customer = models.ForeignKey(CustomUser, on_delete = models.SET_NULL, blank=True, null = True)
+    date_ordered = models.DateTimeField(auto_now_add = True)
+    complete = models.BooleanField(default = False, null=True, blank = True)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self) -> str:
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Item,on_delete=models.SET_NULL, blank = True, null= True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank = True, null = True)
+    quantity = models.IntegerField(default=0, null=True,blank = True)
+    date_added = models.DateTimeField(auto_now_add = True)
