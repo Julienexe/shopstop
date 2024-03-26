dc037f756888a606f00410d2eacf43c35d506fb6
@Alidat <alvinnahereza@gmail.com>
2024-03-26 11:19:03 +0300

    business form

--- shopstop/forms.py
+++ shopstop/forms.py
@@ -1,8 +1,18 @@
+from django import forms
 from django.forms import ModelForm
-from .models import Item
+from .models import Item, Business
 
 #add item form
 class  ItemModelForm(ModelForm):
     class Meta:
         model = Item
         fields=('item_name','price','photos',)
+
+
+class BusinessForm(ModelForm):
+    class Meta:
+        model = Business
+        fields = ['name', 'contact_details', 'location', 'description', 'logo', 'photos', 'videos']
+        widgets = {
+            'description': forms.Textarea(attrs={ 'rows': 5}),  
+        }        
