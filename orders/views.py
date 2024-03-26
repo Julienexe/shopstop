24888dbca2c8a9a4af8d33862e069c415e8f043d
Julian.exe <77165392+Julienexe@users.noreply.github.com>
2024-03-26 20:42:56 +0300

    quantity backend

--- orders/views.py
+++ orders/views.py
@@ -37,13 +37,13 @@ def update_item(request):
     order,created = Order.objects.get_or_create(customer=customer, complete=False)
     order_item,created = OrderItem.objects.get_or_create(order=order, product=product)
     if action == "add":
-        order_item.quantity += 1
+        order_item.quantity = (order_item.quantity+1)
     elif action == "remove":
-        order_item.quantity -= 1
+        order_item.quantity = (order_item.quantity-1)
 
     order_item.save()
 
     if order_item.quantity <= 0:
         order_item.delete()
 
-    return JsonResponse('item added', safe=False)
+    return JsonResponse(action + ' complete', safe=False)
