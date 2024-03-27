e0bfaec286252f37d03b23e09dcf66aeee4c3204
Julian.exe <julianssonko@gmail.com>
2024-03-27 15:22:07 +0000

    configure media

--- config/settings/production.py
+++ config/settings/production.py
@@ -141,7 +141,7 @@ STORAGES = {
     },
 }
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 MEDIA_URL = '/media/'
 
 # Default primary key field type
