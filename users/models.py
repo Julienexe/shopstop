bd6135a1cedc5138b903a923efcfc0c37e93dc58
Julian.exe <julianssonko@gmail.com>
2024-03-27 15:18:46 +0000

    img

--- users/models.py
+++ users/models.py
@@ -29,12 +29,3 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         # save the profile first
         super().save(*args, **kwargs)
-
-        # resize the image
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            # create a thumbnail
-            img.thumbnail(output_size)
-            # overwrite the larger image
-            img.save(self.image.path)
